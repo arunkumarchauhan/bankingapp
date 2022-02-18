4c55809efd6ad1cbe1a6d740f883473e749b9af8
arunkumarchauhan <arunjuly98@gmail.com>
2022-02-18 20:54:24 +0530

    deploy2

--- banking/banking/settings.py
+++ banking/banking/settings.py
@@ -142,4 +142,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_on_heroku
 django_on_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
